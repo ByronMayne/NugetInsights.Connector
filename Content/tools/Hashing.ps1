96223a135daca2ff2e74b517906c57964abe9159
Byron Mayne <Byronmayne@gmail.com>
2025-01-04 15:53:29 -0500

    Added a try catch around hashing to avoid exceptions

--- Content/tools/Hashing.ps1
+++ Content/tools/Hashing.ps1
@@ -1,12 +1,18 @@
 function Get-MachineHash {
-    $machineName = [Environment]::MachineName
-    $userName = [Environment]::UserName
-    $processorCount = [Environment]::ProcessorCount
-    $osVersion = [System.Environment]::OSVersion;
-    $data = "$machineName|$userName|$processorCount|$osVersion"
-    $bytes = [System.Text.Encoding]::UTF8.GetBytes($data)
-    $sha256 = [System.Security.Cryptography.SHA256]::Create()
-    $hashBytes = $sha256.ComputeHash($bytes)
-    $hashString = -join ($hashBytes | ForEach-Object { $_.ToString("x2") })
-    return $hashString
+    try {
+        $machineName = [Environment]::MachineName
+        $userName = [Environment]::UserName
+        $processorCount = [Environment]::ProcessorCount
+        $osVersion = [System.Environment]::OSVersion;
+        $data = "$machineName|$userName|$processorCount|$osVersion"
+        $bytes = [System.Text.Encoding]::UTF8.GetBytes($data)
+        $sha256 = [System.Security.Cryptography.SHA256]::Create()
+        $hashBytes = $sha256.ComputeHash($bytes)
+        $hashString = -join ($hashBytes | ForEach-Object { $_.ToString("x2") })
+        return $hashString
+    }
+    catch 
+    {
+        return 0;
+    }
 }
